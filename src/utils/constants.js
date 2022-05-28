8409c14b5611141e94f39c8f560cd817844085d3
Matias Aquino <m4tias.a@gmail.com>
2022-05-28 18:51:19 -0300

    se añade valores constantes

---
+++ src/utils/constants.js
@@ -0,0 +1,21 @@
+export const STATUS_GAME = ["playing", "winner", "loser"];
+export const ARRAY_KEYBOARD = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I" ,"O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K" ,"L"],
+    ["ENTER", "Z", "X", "C", "V", "B", "N", "M" ,"BORRAR"]
+];
+export const WORDS = [
+    "PLAYA",
+    "MIRAR",
+    "TIRAR",
+    "MEJOR",
+    "VISTA",
+    "JARRA",
+    "AUTOS",
+    "GATO",
+    "LORO",
+    "BAUL",
+    "AUTO",
+    "LIRA",
+
+];
